ec4905e50e6d2637eaac2e2dff92140e3cc5b94d
Karl J. Overå <k@rl.run>
2023-01-16 15:32:46 +0100

    use @slack/bolt

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM gcr.io/distroless/nodejs:16
 
+ENV NODE_ENV production
+
 WORKDIR /app
 COPY dist /app/
 EXPOSE 5000
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,58 @@
-import fastify from 'fastify'
+import { App } from '@slack/bolt'
+import { Logger, LogLevel } from '@slack/logger'
 import logger from './logger'
+import { config } from 'dotenv'
 
-const server = fastify({ logger })
+if (process.env.NODE_ENV !== 'production') {
+    // Load dev environment variables
+    config({})
+}
 
-server.get('/slack/interactivity', async (request) => {
-    logger.info(JSON.stringify(request.body, null, 2))
+const loggerAdapter: Logger = {
+    debug: logger.debug,
+    info: logger.info,
+    warn: logger.warn,
+    error: logger.error,
+    getLevel: (): LogLevel => logger.level as LogLevel,
+    setLevel: (): void => void 0,
+    setName: (): void => void 0,
+}
 
-    return { ok: 'ok' }
+// Initialize Bolt app, using the default HTTPReceiver
+const app = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    logger: loggerAdapter,
+    customRoutes: [
+        {
+            path: '/internal/is_alive',
+            method: ['GET'],
+            handler: (req, res) => {
+                res.writeHead(200)
+                res.end('OK')
+            },
+        },
+        {
+            path: '/internal/is_ready',
+            method: ['GET'],
+            handler: (req, res) => {
+                res.writeHead(200)
+                res.end('OK')
+            },
+        },
+    ],
 })
 
-server.get('/internal/is_alive', async () => {
-    return { ok: 'ok' }
+// Listens for messages containing "knock knock" and responds with an italicized "who's there?"
+app.message('knock knock', async ({ message, say }) => {
+    logger.info(JSON.stringify(message))
+    await say(`_Who's there?_`)
 })
 
-server.get('/internal/is_ready', async () => {
-    return { ok: 'ok' }
-})
+async function start() {
+    const port = process.env.PORT || 5000
+    await app.start(port)
+    logger.info(`Started bolt app on port ${port}`)
+}
 
-server.listen({ port: 5000, host: '0.0.0.0' }, (err) => {
-    if (err) {
-        console.error(err)
-        process.exit(1)
-    }
-})
-
+start()
