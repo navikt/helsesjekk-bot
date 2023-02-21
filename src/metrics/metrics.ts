11881cc98de6c9bf828804d366ddba0f964b9231
Karl J. Overå <k@rl.run>
2023-02-21 12:15:47 +0100

    add diff from previous weeks ask

--- src/metrics/metrics.ts
+++ src/metrics/metrics.ts
@@ -1,36 +1,51 @@
 import * as R from 'remeda'
-import { Answer, Asked } from '@prisma/client'
+import { Answer } from '@prisma/client'
 
 import { answerFromJsonb, questionsFromJsonb } from '../questions/jsonb-utils'
-import { AnswerLevel, QuestionAnswer } from '../db'
+import { AnswerLevel, AskedWithAnswers, QuestionAnswer } from '../db'
 
 export interface ScoredQuestion {
+    id: string
     score: number
     question: string
 }
 
-export function scoreQuestions(asked: Asked & { answers: Answer[] }): ScoredQuestion[] {
+export type ScoredAsk = {
+    totalScore: number
+    scoredQuestions: ScoredQuestion[]
+}
+
+export function scoreAsked(asked: AskedWithAnswers): ScoredAsk {
     const questions = questionsFromJsonb(asked.questions)
 
-    const answersByQuestionId = R.pipe(
-        asked.answers,
-        R.map(R.prop('answers')),
-        R.flatMap(answerFromJsonb),
-        R.groupBy(R.prop('questionId')),
-    )
-
-    return questions.map((it) => {
+    const answersByQuestionId = groupAnswersByQuestionId(asked.answers)
+    const scoredQuestions = questions.map((it) => {
         const answers = answersByQuestionId[it.questionId]
-        const score = R.pipe(answers, R.map(answerToValue), R.sumBy(R.identity), (it) => it / answers.length)
+        const score = scoreAnswers(answers)
 
         return {
+            id: it.questionId,
             question: it.question,
             score,
         }
     })
+
+    return {
+        totalScore: overallScore(scoredQuestions),
+        scoredQuestions,
+    }
 }
 
-export function overallScore(scoredQuestions: ScoredQuestion[]): number {
+const groupAnswersByQuestionId: (answers: Answer[]) => Record<string, QuestionAnswer[]> = R.createPipe(
+    R.map(R.prop('answers')),
+    R.flatMap(answerFromJsonb),
+    R.groupBy(R.prop('questionId')),
+)
+
+const scoreAnswers = (answers: QuestionAnswer[]): number =>
+    R.pipe(answers, R.map(answerToValue), R.sumBy(R.identity), (it) => it / answers.length)
+
+function overallScore(scoredQuestions: ScoredQuestion[]): number {
     return R.pipe(scoredQuestions, R.map(R.prop('score')), R.sumBy(R.identity), (it) => it / scoredQuestions.length)
 }
 
