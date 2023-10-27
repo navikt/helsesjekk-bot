1dc134c97722268925ae9d9bad689ecb74e1ddeb
Karl J. Overå <k@rl.run>
2023-10-27 15:30:37 +0200

    get user from token

--- src/components/LoggedInUser.tsx
+++ src/components/LoggedInUser.tsx
@@ -1,20 +1,20 @@
 import React, { ReactElement } from 'react'
 
+import { getUser } from '../auth/authentication'
+
 import { Skeleton, BodyShort, Detail } from 'aksel-server'
 
 async function LoggedInUser(): Promise<ReactElement> {
-    const loggedInUser: { name: string; email: string } = await new Promise((resolve) =>
-        setTimeout(() => resolve({ name: 'Test user', email: 'user@nav.no' }), 1000),
-    )
+    const user = getUser()
 
     return (
         <div className="flex gap-4 p-4">
-            <div>
-                <BodyShort>{loggedInUser.name}</BodyShort>
-                <Detail>{loggedInUser.email}</Detail>
+            <div className="text-right">
+                <BodyShort>{user.name}</BodyShort>
+                <Detail className="whitespace-nowrap">{user.email}</Detail>
             </div>
             <div className="w-[48px] h-[48px] bg-gray-400 rounded-full flex items-center justify-center text-2xl">
-                {loggedInUser.name[0]}
+                {user.name[0]}
             </div>
         </div>
     )
