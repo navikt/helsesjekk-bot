2cf9c914d1bc7a2f4c4ddd99529e63563dab27f5
Karl J. Overå <k@rl.run>
2023-10-27 15:59:57 +0200

    get team by ad association

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,26 @@
 import { ReactElement } from 'react'
 import { headers } from 'next/headers'
 
-import { verifyUserLoggedIn } from '../auth/authentication'
+import { getUser, verifyUserLoggedIn } from '../auth/authentication'
+import { getTeamByAdGroup } from '../db'
+
+import { Heading } from 'aksel-server'
 
 export default async function Page(): Promise<ReactElement> {
     await verifyUserLoggedIn('/')
-
     const authHeader = headers().get('authorization')
 
+    const assosiatedTeam = await getTeamByAdGroup(getUser().adGroups)
+
     return (
-        <div>
-            <h1>Page</h1>
-            <p>Authorization header: {authHeader ?? 'Missing'}</p>
+        <div className="container mx-auto">
+            <Heading size="large">Your team</Heading>
+            {assosiatedTeam ? (
+                <pre>{JSON.stringify(assosiatedTeam, null, 2)}</pre>
+            ) : (
+                <p>You are not a member of any team</p>
+            )}
+            <pre className="mt-16">Authorization header: {authHeader ?? 'Missing'}</pre>
         </div>
     )
 }
