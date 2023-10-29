1f100d86f6e2070686ee06cc2392fc010515517a
Karl J. Overå <k@rl.run>
2023-10-29 15:16:48 +0100

    add team active-status on card

--- src/components/TeamCard.tsx
+++ src/components/TeamCard.tsx
@@ -5,6 +5,7 @@ import { Team } from '../db'
 import { dayIndexToDay } from '../utils/date'
 
 import { Heading, BodyShort, Detail, LinkPanel } from 'aksel-server'
+import { GavelIcon, QuestionmarkIcon, Tooltip } from 'aksel-client'
 
 type Props = {
     team: Team
@@ -12,21 +13,51 @@ type Props = {
 
 function TeamCard({ team }: Props): ReactElement {
     return (
-        <LinkPanel as={Link} href={`/team/${team.assosiatedGroup}`} className="max-w-sm grow">
-            <Heading size="small">{team.name}</Heading>
-            <div>
-                <Detail>Spør på</Detail>
-                <BodyShort>
-                    {dayIndexToDay(team.postDay)} kl. {team.postHour}:00
-                </BodyShort>
-            </div>
-            <div>
-                <Detail>Viser svar</Detail>
-                <BodyShort>
-                    {dayIndexToDay(team.revealDay)} kl. {team.revealHour}:00
-                </BodyShort>
+        <LinkPanel as={Link} href={`/team/${team.assosiatedGroup}`} className="max-w-sm grow relative">
+            <Tooltip content={`Teamet er ${team.active ? 'aktivt' : 'deaktivert'}`}>
+                <div className="flex gap-3 items-center">
+                    {team.active ? <PingDot /> : <InactiveDot />}
+                    <Heading size="small">{team.name}</Heading>
+                </div>
+            </Tooltip>
+            <div className="flex gap-4 flex-wrap">
+                <div>
+                    <Detail className="flex gap-1 items-center">
+                        <QuestionmarkIcon aria-hidden />
+                        Spør på
+                    </Detail>
+                    <BodyShort>
+                        {dayIndexToDay(team.postDay)} kl. {team.postHour}:00
+                    </BodyShort>
+                </div>
+                <div>
+                    <Detail className="flex gap-1 items-center">
+                        <GavelIcon aria-hidden />
+                        Viser svar
+                    </Detail>
+                    <BodyShort>
+                        {dayIndexToDay(team.revealDay)} kl. {team.revealHour}:00
+                    </BodyShort>
+                </div>
             </div>
         </LinkPanel>
+    )
+}
+
+function PingDot(): ReactElement {
+    return (
+        <span className="relative flex h-3 w-3">
+            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
+            <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
+        </span>
+    )
+}
+
+function InactiveDot(): ReactElement {
+    return (
+        <span className="relative flex h-3 w-3">
+            <span className="relative inline-flex rounded-full h-3 w-3 bg-red-300"></span>
+        </span>
     )
 }
 
