0d5a6c0716d38929d7e63a62ecb04ae3273e73d1
Karl J. Overå <k@rl.run>
2023-11-09 15:18:16 +0100

    fix: handle old ingress in middleware from nais headers

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,16 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import { logger } from '@navikt/next-logger'
 
 import { browserEnv, isLocal } from './utils/env'
 
 export function middleware(request: NextRequest): NextResponse | void {
     const url = new URL(request.url)
+    const forwardedHostHeader = request.headers.get('x-forwarded-host')
 
     // Redirect to new ingress in production env
-    if (browserEnv.NEXT_PUBLIC_ENVIRONMENT === 'production' && url.host.includes('intern')) {
+    if (browserEnv.NEXT_PUBLIC_ENVIRONMENT === 'production' && forwardedHostHeader.includes('intern')) {
+        logger.info('Hit old ingress, redirecting to new ingress')
         return NextResponse.redirect(new URL(url.pathname, 'https://helsesjekk-bot.nav.no/'))
     }
 
