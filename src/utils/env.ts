131833d176b8d5dd147558b03da8c43c0f5ff187
ninhqd <83956288+ninhqd@users.noreply.github.com>
2023-11-09 10:30:24 +0100

    Update env.ts

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -9,13 +9,7 @@ export type ServerEnv = z.infer<typeof serverEnvSchema>
 export const serverEnvSchema = z.object({
     SLACK_SIGNING_SECRET: z.string(),
     SLACK_BOT_TOKEN: z.string(),
-    SLACK_APP_TOKEN: z.string(),
-    // Provided my nais
-    AZURE_APP_CLIENT_ID: z.string(),
-    AZURE_APP_CLIENT_SECRET: z.string(),
-    AZURE_OPENID_CONFIG_TOKEN_ENDPOINT: z.string(),
-    AZURE_APP_WELL_KNOWN_URL: z.string(),
-    AZURE_APP_PRE_AUTHORIZED_APPS: z.string(),
+    SLACK_APP_TOKEN: z.string()
 })
 
 export const browserEnv = publicEnvSchema.parse({
@@ -27,13 +21,7 @@ const getRawServerConfig = (): Partial<unknown> =>
         // Provided by nais-*.yml secrets
         SLACK_SIGNING_SECRET: process.env.SLACK_SIGNING_SECRET,
         SLACK_BOT_TOKEN: process.env.SLACK_BOT_TOKEN,
-        SLACK_APP_TOKEN: process.env.SLACK_APP_TOKEN,
-        // Provided by nais
-        AZURE_APP_CLIENT_ID: process.env.AZURE_APP_CLIENT_ID,
-        AZURE_APP_CLIENT_SECRET: process.env.AZURE_APP_CLIENT_SECRET,
-        AZURE_OPENID_CONFIG_TOKEN_ENDPOINT: process.env.AZURE_OPENID_CONFIG_TOKEN_ENDPOINT,
-        AZURE_APP_WELL_KNOWN_URL: process.env.AZURE_APP_WELL_KNOWN_URL,
-        AZURE_APP_PRE_AUTHORIZED_APPS: process.env.AZURE_APP_PRE_AUTHORIZED_APPS,
+        SLACK_APP_TOKEN: process.env.SLACK_APP_TOKEN
     }) satisfies Record<keyof ServerEnv, string | undefined>
 
 /**
