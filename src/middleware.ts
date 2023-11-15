abfadc45b34d0ef3622c7f45c4fb37f7f76ed3db
Ninh Qui Do <doquininh@gmail.com>
2023-11-15 13:46:55 +0100

    middleware file

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { logger } from '@navikt/next-logger'
+
+import { browserEnv, isLocal } from './utils/env'
+
+export function middleware(request: NextRequest): NextResponse | void {
+    const url = new URL(request.url)
+    const forwardedHostHeader = request.headers.get('x-forwarded-host')
+
+    // Redirect to new ingress in production env
+    if (browserEnv.NEXT_PUBLIC_ENVIRONMENT === 'production' && forwardedHostHeader.includes('intern')) {
+        logger.info('Hit old ingress, redirecting to new ingress')
+        return NextResponse.redirect(new URL(url.pathname, 'https://helsesjekk-bot.bks-prod.politiet.no/'))
+    }
+
+    // Make sure everyone is authed now that it's on a public ingress
+    if (!isLocal && !request.headers.has('Authorization')) {
+        return NextResponse.redirect(new URL(`/oauth2/login?redirect=${url.pathname}`, request.url))
+    }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+}
