1fdd8eec09be37024459d8e6b0128ccdbb63633d
Ninh Qui Do <doquininh@gmail.com>
2023-11-20 15:57:21 +0100

    callback

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,13 +1,22 @@
-import NextAuth from 'next-auth'
-import AzureADProvider from 'next-auth/providers/azure-ad'
+import NextAuth from "next-auth";
+import AzureADProvider from "next-auth/providers/azure-ad";
 export const authOptions = {
-    providers: [
-        AzureADProvider({
-            clientId: process.env.AZURE_APP_CLIENT_ID,
-            clientSecret: process.env.AZURE_APP_CLIENT_SECRET,
-            tenantId: process.env.AZURE_APP_TENANT_ID,
-        }),
-    ],
-    secret: process.env.NEXTAUTH_SECRET
-}
-export default NextAuth(authOptions)
+  providers: [
+    AzureADProvider({
+      clientId: process.env.AZURE_APP_CLIENT_ID,
+      clientSecret: process.env.AZURE_APP_CLIENT_SECRET,
+      tenantId: process.env.AZURE_APP_TENANT_ID,
+      authorization: { params: { scope: "openid profile user.Read email" } },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      // IMPORTANT: Persist the access_token to the token right after sign in
+      if (account) {
+        token.idToken = account.id_token;
+      }
+      return token;
+    },
+  }
+};
+export default NextAuth(authOptions);
