a524b39335d3541ed836983b1cf304e5900cfd3d
Ninh Qui Do <doquininh@gmail.com>
2023-11-20 16:50:42 +0100

    remove bot debug

--- src/bot/app.ts
+++ src/bot/app.ts
@@ -1,22 +1,22 @@
-import { LogLevel } from '@slack/logger'
+import { Logger, LogLevel } from '@slack/logger'
 import { App as BoltApp } from '@slack/bolt'
 import { lazyNextleton } from 'nextleton'
 import { ProxyAgent } from 'proxy-agent';
 
-// import { logger } from '@navikt/next-logger'
+import { logger } from '@navikt/next-logger'
 
-// const slackLogger = logger.child({ x_context: 'slack-bot', x_isSlack: true })
+const slackLogger = logger.child({ x_context: 'slack-bot', x_isSlack: true })
 
 // Custom logger adapter because Bolt didn't like the pino logger
-// const loggerAdapter: Logger = {
-//     debug: (msg) => slackLogger.debug(msg),
-//     info: (msg) => slackLogger.info(msg),
-//     warn: (msg) => slackLogger.warn(msg),
-//     error: (msg) => slackLogger.error(msg),
-//     getLevel: (): LogLevel => slackLogger.level as LogLevel,
-//     setLevel: (): void => void 0,
-//     setName: (): void => void 0,
-// }
+const loggerAdapter: Logger = {
+    debug: (msg) => slackLogger.debug(msg),
+    info: (msg) => slackLogger.info(msg),
+    warn: (msg) => slackLogger.warn(msg),
+    error: (msg) => slackLogger.error(msg),
+    getLevel: (): LogLevel => slackLogger.level as LogLevel,
+    setLevel: (): void => void 0,
+    setName: (): void => void 0,
+}
 
 const app = lazyNextleton(
     'bolt',
@@ -27,8 +27,7 @@ const app = lazyNextleton(
             signingSecret: process.env.SLACK_SIGNING_SECRET,
             appToken: process.env.SLACK_APP_TOKEN,
             agent: new ProxyAgent(),
-            // logger: loggerAdapter,
-            logLevel: LogLevel.DEBUG,
+            logger: loggerAdapter,
         }),
 )
 
