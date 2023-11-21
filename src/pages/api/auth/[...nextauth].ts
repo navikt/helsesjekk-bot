47db1251c4a1f701f9455179a3c45ea6a84c6e8a
Ninh Qui Do <doquininh@gmail.com>
2023-11-21 12:00:25 +0100

    patch in proxy agent

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,5 @@
 import NextAuth, { AuthOptions } from "next-auth";
 import AzureADProvider from "next-auth/providers/azure-ad";
-import { HttpsProxyAgent } from "https-proxy-agent";
 
 export const authOptions: AuthOptions = {
 
@@ -9,9 +8,6 @@ export const authOptions: AuthOptions = {
       clientId: process.env.AZURE_APP_CLIENT_ID,
       clientSecret: process.env.AZURE_APP_CLIENT_SECRET,
       tenantId: process.env.AZURE_APP_TENANT_ID,
-      httpOptions: {
-        agent: new HttpsProxyAgent(process.env.HTTP_PROXY),
-      },
     }),
   ],
   debug: true,
