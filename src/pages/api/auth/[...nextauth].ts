8e606ab9a336655406cb4d088ede90e6d02bbaca
Ninh Qui Do <doquininh@gmail.com>
2023-11-21 10:37:50 +0100

    proxy agent

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,28 +1,19 @@
 import NextAuth, { AuthOptions } from "next-auth";
 import AzureADProvider from "next-auth/providers/azure-ad";
+import { ProxyAgent } from 'proxy-agent';
+
 export const authOptions: AuthOptions = {
+
   providers: [
     AzureADProvider({
       clientId: process.env.AZURE_APP_CLIENT_ID,
       clientSecret: process.env.AZURE_APP_CLIENT_SECRET,
       tenantId: process.env.AZURE_APP_TENANT_ID,
-      client: {
-        redirect_uris: [process.env.CALLBACK_URL]
+      httpOptions: {
+        agent: new ProxyAgent(),
       }
     }),
   ],
-  callbacks: {
-    async signIn({ user, account, profile, email, credentials }) {
-      return true;
-    },
-    async jwt({ token, account }) {
-      // IMPORTANT: Persist the access_token to the token right after sign in
-      if (account) {
-        token.idToken = account.id_token;
-      }
-      return token;
-    },
-  },
   debug: true,
 };
 export default NextAuth(authOptions);
