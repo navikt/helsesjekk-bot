06125651df8f668f971c9b2f18a4d4e99fd797ee
Ninh Qui Do <doquininh@gmail.com>
2023-11-21 18:13:04 +0100

    fix

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth, { AuthOptions } from "next-auth";
 import AzureADProvider from "next-auth/providers/azure-ad";
 
-export const authOptions: AuthOptions = {
+const authOptions: AuthOptions = {
 
   providers: [
     AzureADProvider({
