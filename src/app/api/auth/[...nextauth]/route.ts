01fd1826dc60603b151c18159c10856c2ed7798b
Ninh Qui Do <doquininh@gmail.com>
2023-11-22 19:04:00 +0100

    try not using built in provider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,19 +3,39 @@ import AzureADProvider from "next-auth/providers/azure-ad";
 
 const authOptions: AuthOptions = {
   providers: [
-    AzureADProvider({
-      clientId: process.env.AZURE_APP_CLIENT_ID,
-      clientSecret: process.env.AZURE_APP_CLIENT_SECRET,
-      tenantId: process.env.AZURE_APP_TENANT_ID,
-    }),
+    {
+      id: 'azure-ad',
+      name: 'Azure Active Directory',
+      type: 'oauth',
+      authorization: {
+        url: `https://login.microsoftonline.com/${process.env.AZURE_APP_TENANT_ID}/oauth2/v2.0/authorize?response_mode=query`,
+        params: {
+          audience: process.env.AZURE_APP_CLIENT_ID,
+          scope:
+            'openid',
+        },
+      },
+      token: `https://login.microsoftonline.com/${process.env.AZURE_APP_TENANT_ID}/oauth2/v2.0/token`,
+      wellKnown: process.env.AZURE_APP_OPENID,
+      profile(profile) {
+        return {
+          id: profile.sub,
+          ...profile,
+        };
+      },
+      options: {
+        clientId: process.env.AZURE_APP_CLIENT_ID,
+        clientSecret: process.env.AZURE_APP_CLIENT_SECRET,
+      },
+    },
   ],
   callbacks: {
     async signIn({ user, account, profile, email, credentials }) {
-      console.info(`SIGN IN - PROFILE ${profile.name}`);
+      console.info(`SIGN IN`);
       return true;
     },
     async redirect({ url, baseUrl }) {
-      console.info(`REDIRECT - URL ${url} - BASEURL ${baseUrl}`);
+      console.info(`REDIRECT`);
       return baseUrl;
     },
     async session({ session, user, token }) {
