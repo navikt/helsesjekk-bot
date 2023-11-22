49e0028ff387766d77237c58d166c1732cd9a489
Ninh Qui Do <doquininh@gmail.com>
2023-11-22 20:50:25 +0100

    debug

--- src/auth/authentication.tsx
+++ src/auth/authentication.tsx
@@ -22,6 +22,7 @@ export async function validateToken(redirectPath: string): Promise<void> {
         return
     }
     const session = await getServerSession();
+    console.info(session);
 
     const bearerToken: string | null | undefined = session.accessToken;
     if (!bearerToken) {
