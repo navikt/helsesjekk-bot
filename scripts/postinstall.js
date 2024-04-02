cc7d244b638748581b260c4423c91dd06beb22b7
Karl J. Overå <k@rl.run>
2024-04-02 15:28:07 +0200

    chore(deps): skippertak-bump

--- scripts/postinstall.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-
-const isCi = process.env.CI !== undefined
-const isProduction = process.env.NODE_ENV === 'production'
-
-if (!(isCi || isProduction)) {
-    require('husky').install()
-}
