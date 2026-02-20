ea1d1f0873c63b02bcc3c1eb43cc0b57647db10e
Karl J. Overå <k@rl.run>
2026-02-20 08:39:57 +0100

    feat: new question categories

--- src/utils/asked.ts
+++ src/utils/asked.ts
@@ -6,8 +6,14 @@ export function questionTypeToText(type: QuestionType): string {
             return 'Meg'
         case QuestionType.SURROUNDINGS:
             return 'Omgivelsene'
+        case QuestionType.TEMPERATURE:
+            return 'Temperaturmåling'
         case QuestionType.TEAM_HEALTH:
             return 'Teamhelse'
+        case QuestionType.SECTION_HEALTH:
+            return 'Seksjonshelse'
+        case QuestionType.GROUP_HEALTH:
+            return 'Gruppehelse'
         case QuestionType.SPEED:
             return 'Fart & flyt'
         case QuestionType.TECH:
